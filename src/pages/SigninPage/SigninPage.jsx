77727e69ab5123219928541f57f1d9349d3535c1
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-12-12 00:14:09 +0530

    login edits

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -45,7 +45,7 @@ const SigninPage = () => {
         navigate("/dashboard-user");
       }, 4000);
     } catch (error) {
-      console.log(error);
+      console.log(error?.response?.data?.message);
       // toast.error(error.response.data.message);
     }
   };
