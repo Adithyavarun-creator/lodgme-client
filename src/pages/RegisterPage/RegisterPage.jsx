6a8b1a6839393b0622ea25deaae9d64f2fc93b1a
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-12-11 23:39:02 +0530

    register link added

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -46,28 +46,28 @@ const RegisterPage = () => {
     }
     try {
       setLoading(true);
-      const res = await axios.post(
-        `${process.env.REACT_APP_BACKEND_URL}/api/register`,
-        {
-          firstname,
-          lastname,
-          email,
-          gender,
-          location,
-          contactnumber,
-          password,
-        }
-      );
-      // const res = await registerUser({
-      //   firstname,
-      //   lastname,
-      //   email,
-      //   gender,
-      //   location,
-      //   contactnumber,
-      //   password,
-      // });
-      console.log(res.data);
+      // const res = await axios.post(
+      //   `${process.env.REACT_APP_BACKEND_URL}/api/register`,
+      //   {
+      //     firstname,
+      //     lastname,
+      //     email,
+      //     gender,
+      //     location,
+      //     contactnumber,
+      //     password,
+      //   }
+      // );
+      const res = await registerUser({
+        firstname,
+        lastname,
+        email,
+        gender,
+        location,
+        contactnumber,
+        password,
+      });
+      //console.log(res.data);
       toast("You have been registered in LodgeMe community!", {
         icon: "👏",
       });
