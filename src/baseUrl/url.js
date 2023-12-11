2d4eae7fe17638ffe8a6d802805f8ca8a2e0a459
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-12-11 23:37:34 +0530

    new url pushes

--- src/baseUrl/url.js
+++ src/baseUrl/url.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseUrl = "";
+const baseUrl = "https://server-api-q17g.onrender.com";
 
 export const registerUser = async (user) => {
-  await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/register`, user);
+  await axios.post(`${baseUrl}/api/register`, user);
 };
 
 export const loginUser = async (user) => {
