9394bbb933b7f155f5ee60c1b2dfef7b3ba7f724
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-12-12 15:08:49 +0530

    updated

--- src/baseUrl/url.js
+++ src/baseUrl/url.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "https://server-api-q17g.onrender.com"; http://localhost:8000
-export const baseUrl = "https://server-api-q17g.onrender.com";
+export const baseUrl = "http://localhost:8000";
