f3670a340bce6d5522436981f6b316ec0608e563
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-12-27 20:01:15 +0530

    vercel backend added

--- src/baseUrl/url.js
+++ src/baseUrl/url.js
@@ -1,8 +1,9 @@
 // export const baseUrl = "https://server-api-q17g.onrender.com";
 // http://localhost:8000
+//https://lodgme-server-api.vercel.app
 import axios from "axios";
 
-export const baseUrl = "https://server-api-q17g.onrender.com";
+export const baseUrl = "https://lodgme-server-api.vercel.app";
 
 export const searchListings = async (query) => {
   await axios.post(`${baseUrl}/api/search-listings`, query);
